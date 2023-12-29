5e168e1e9588f4e5ed2767076b71895c7bd3b192
Bartosz Złotnik <bartekzlotnik@gmail.com>
2023-12-30 00:09:53 +0100

    ADD(rumps menu bar with icon)

--- src/automatic_translator.py
+++ src/automatic_translator.py
@@ -1,16 +1,39 @@
 import openai
 import os
+import pathlib
 from dotenv import load_dotenv
 from pynput import keyboard
 import subprocess
+import rumps
 
 load_dotenv()
 LANGUAGE = os.getenv('TRANSLATE_LAN')
 KEY = os.getenv('API_KEY')
 SYSTEM = os.name
+ICON_PATH = pathlib.Path(__file__).parent.parent / "assets" / "icon.png"
 
 #There you can specifty your shortcut for translation of the clipboard [Default: CTRL + SHIFT + T]
 COMBINATION = {keyboard.Key.ctrl, keyboard.Key.shift, keyboard.KeyCode.from_char('t')}
+
+
+class macOS_app(rumps.App):
+
+    def __init__(self):
+
+        super(macOS_app, self).__init__("AiTranslator", icon=str(ICON_PATH))
+        self.menu = ["Select Language", "Insert GPT-KEY"]
+
+
+    @rumps.clicked("Insert GPT-KEY")
+    def inserting_api_key(self, _):
+
+        response = rumps.Window(title="Insert GPT API KEY: ", ok="ok", cancel="cancel", dimensions=(100, 20)).run()
+
+        if response.clicked:
+
+            input_text = response.text
+            print(input_text)
+
 
 pressed_keys = set()
 
@@ -80,7 +103,6 @@ def pressing_buttons(key):
                 copying_to_clipboard_win(translated)
 
 
-
 def releasing_buttons(key):
 
     try:
@@ -100,14 +122,17 @@ def ai_translation(clipboard: str, language: str):
             messages=[
             {"role": "user", "content": f"As my assistant please translate: {clipboard} to {language}. Please leave only clear translated text without any definitions"}]
         )
-        
+
     except:
         print("There is a problem with the API key, please check its validity.")
         return ""
     
     return response.choices[0].message.content.strip()
 
-with keyboard.Listener(on_press=pressing_buttons, on_release=releasing_buttons) as keyboard_listener:
+# with keyboard.Listener(on_press=pressing_buttons, on_release=releasing_buttons) as keyboard_listener:
 
-    keyboard_listener.join()
+#     keyboard_listener.join()
 
+if __name__ == "__main__":
+    app = macOS_app()
+    app.run()
